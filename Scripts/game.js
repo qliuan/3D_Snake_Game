8d9e1e28c5f94850fd904be5f692e0556454b261
JayLIU0911 <jasonliu9975@gmail.com>
2017-11-23 14:12:48 +0800

    second commit

--- Scripts/game.js
+++ Scripts/game.js
@@ -1,7 +1,10 @@
 var renderer, camera, scene;
 
-var planeWidth = 640;
-var planeHeight = 360;
+var planeWidth = 10000;
+var planeHeight = 10000;
+
+var sunRadius = 5000;
+var sun, lantern;
 
 var STEP = 3;
 var head;
@@ -50,7 +53,7 @@ function createScene()
 
 	var	planeQuality = 10;
   var planeMaterial =
-	  new THREE.MeshLambertMaterial(
+	  new THREE.MeshPhongMaterial(
 		{
 		  color: 0x4BD121
 		});
@@ -63,28 +66,56 @@ function createScene()
 	  planeMaterial);
 	scene.add(plane);
 
+  plane.receiveShadow = true;
+  plane.castShadow = true;
+
+  var loader = new THREE.OBJLoader();
+
+
+
   var snakeMaterial =
-	  new THREE.MeshLambertMaterial(
+	  new THREE.MeshPhongMaterial(
 		{
 		  color: 0x1B32C0
 		});
   for (var i = 0; i < bodyLength; ++i) {
-    var bodyBlock = new THREE.Mesh(
-  	  new THREE.CubeGeometry(
-  		STEP,
-  		STEP,
-  		STEP,
-  		1,
-  		1,
-  		1),
-  	  snakeMaterial);
-    bodyBlock.position.x = startPosition[0] - STEP - i * STEP;
-    bodyBlock.position.y = startPosition[1];
-  	bodyBlock.position.z = startPosition[2];
-  	scene.add(bodyBlock);
+    // var bodyBlock = new THREE.Mesh(
+  	//   new THREE.CubeGeometry(
+  	// 	STEP,
+  	// 	STEP,
+  	// 	STEP,
+  	// 	1,
+  	// 	1,
+  	// 	1),
+  	//   snakeMaterial);
+    loader.load(
+    // resource URL
+    'data/cube_bumpy.obj',
+    // called when resource is loaded
+    function ( bodyBlock ) {
+      bodyBlock.position.x = startPosition[0] - STEP - i * STEP;
+      bodyBlock.position.y = startPosition[1];
+    	bodyBlock.position.z = startPosition[2];
+    	scene.add(bodyBlock);
 
-    body.push(bodyBlock);
-  	// bodyBlock.receiveShadow = true;
+      body.push(bodyBlock);
+    	bodyBlock.receiveShadow = true;
+      bodyBlock.castShadow = true;
+    },
+    // called when loading is in progresses
+    function ( xhr ) {
+
+      console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+    },
+    // called when loading has errors
+    function ( error ) {
+
+      console.log( 'An error happened' );
+
+    }
+    );
+
   }
 
   var radius = STEP/2,
@@ -92,7 +123,7 @@ function createScene()
 		rings = 6;
 
 	var sphereMaterial =
-	  new THREE.MeshLambertMaterial(
+	  new THREE.MeshPhongMaterial(
 		{
 		  color: 0xD43001
 		});
@@ -109,15 +140,33 @@ function createScene()
 	head.position.y = startPosition[1];
 	head.position.z = startPosition[2];
 
-  pointLight =
-    new THREE.PointLight(0xF8D898);
+  head.receiveShadow = true;
+  head.castShadow = true;
 
-  pointLight.position.x = -1000;
-  pointLight.position.y = 0;
-  pointLight.position.z = 1000;
-  pointLight.intensity = 2.9;
-  pointLight.distance = 10000;
-  scene.add(pointLight);
+  // pointLight =
+  //   new THREE.PointLight(0xF8D898);
+  //
+  // pointLight.position.x = 0;
+  // pointLight.position.y = 0;
+  // pointLight.position.z = 1000;
+  // pointLight.intensity = 2.9;
+  // pointLight.distance = 10000;
+  // scene.add(pointLight);
+
+  sun = new THREE.SpotLight(0xffffff);
+  sun.position.set(0, -sunRadius, 0);
+  sun.intensity = 3;
+  sun.castShadow = true;
+  scene.add(sun);
+
+  lantern = new THREE.SpotLight(0xffffff);
+  lantern.position.set(startPosition[0], startPosition[1], startPosition[2] + STEP * 3);
+  lantern.intensity = 5;
+  lantern.castShadow = true;
+  scene.add(lantern);
+
+  // MAGIC SHADOW CREATOR DELUXE EDITION with Lights PackTM DLC
+  renderer.shadowMapEnabled = true;
 
   console.log(scene);
 }
@@ -127,6 +176,15 @@ var now;
 var then = Date.now();
 var interval = 1000/fps;
 var delta;
+
+var time = 0;
+
+function updateLight() {
+  sun.position.y = sunRadius * Math.sin(time/10);
+  sun.position.z = sunRadius * Math.cos(time/10);
+
+  lantern.position.set(head.position.x, head.position.y, head.position.z + STEP * 3);
+}
 
 function draw()
 {
@@ -144,5 +202,7 @@ function draw()
 		bodyMovement();
 		headMovement();
 		updateCamera();
+    updateLight();
+    time++;
 	}
 }
